5df23c385c2bbaed57fb4221090149d0abb26047
MuhammadNauman400 <naumanmuhammad663@gmail.com>
2026-01-15 16:01:54 +0500

    Added Update and Delete Review with sweet alerts  functionality

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Backend\ReviewController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -37,5 +38,15 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
+
+Route::controller(ReviewController::class)->group(function(){
+    Route::get('/all/review', 'AllReview')->name('all.review');
+    Route::get('/add/review', 'AddReview')->name('add.review');
+    Route::post('/store/review', 'StoreReview')->name('store.review');
+    Route::get('/edit/review/{id}', 'EditReview')->name('edit.review');
+    Route::post('/update/review', 'UpdateReview')->name('update.review');
+    Route::get('/delete/review/{id}', 'DeleteReview')->name('delete.review');
     
+});
+
 });
