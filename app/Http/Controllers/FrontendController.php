28fa5d9fdaa0ba4a74e41dff195086dfa74fbf60
MuhammadNauman400 <naumanmuhammad663@gmail.com>
2026-01-24 19:15:32 +0500

    Contact Page setup, send message from frontend, store in db and show on backend admin dashboard and Delete message functionality too.

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\BlogCategory;
 use App\Models\BlogPost;
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
@@ -46,5 +47,44 @@ class FrontendController extends Controller
         $blogcat = BlogCategory::latest()->withCount('posts')->get();
         $recentpost = BlogPost::latest()->limit(3)->get();
         return view('home.blog.blog_category', compact('blog', 'categoryname', 'blogcat', 'recentpost'));
+    }
+
+    public function ContactUs()
+    {
+        return view('home.contact.contact_us');
+    }
+
+    public function ContactMessage(Request $request)
+    {
+        Contact::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'message' => $request->message,
+        ]);
+
+        $notification = array(
+            'message' => 'Your Message Sent Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+    }
+
+    public function ContactAllMessage()
+    {
+        $message = Contact::latest()->get();
+        return view('admin.backend.contact.all_message', compact('message'));
+    }
+
+    public function DeleteMessage($id)
+    {
+        Contact::find($id)->delete();
+
+        $notification = array(
+            'message' => 'Contact Message Deleted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
     }
 }
